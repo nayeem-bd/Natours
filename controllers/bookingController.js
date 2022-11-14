07ad0f437c93ac69ffa2658ba7a19facaf2c21cc
Nayeem <nimuzzamanj@gmail.com>
2022-11-14 22:15:55 +0600

    fix webhooks major

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -65,8 +65,7 @@ exports.webhookCheckout = (req,res,next)=>{
     const signature = req.headers['stripe-signature'];
     let event;
     try{
-        event = stripe.webhooks.constructEvent(req.body,signature,process.env.STRIPE_WEBHOOK_SECRET);
-
+        event = stripe.webhooks.constructEvent(req.rawBody,signature,process.env.STRIPE_WEBHOOK_SECRET);
     }catch(err){
         return res.status(400).send(`webhook error: ${err.message}`);
     }
