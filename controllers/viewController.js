e05a370914a71d9f2b552eceeafa52c5ded8eb26
Nayeem <nimuzzamanj@gmail.com>
2022-11-15 14:50:11 +0600

    added alert on tour booking

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,6 +4,12 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+exports.alerts = (req,res,next)=>{
+    const {alert} = req.query;
+    if(alert==='booking')
+        res.locals.alert = 'Your booking was successful! Please check your email for a comfirmation';
+    next();
+};
 
 exports.getOverview = catchAsync(async (req,res,next)=>{
     // 1) get tour data from collection
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(viewController.alerts);
 
 router.get('/',/*bookingController.createBookingCheckout,*/authController.isLoggedIn,viewController.getOverview);
 router.get('/tour/:slug',authController.isLoggedIn,viewController.getTour);
