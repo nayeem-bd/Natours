6e7e242a281d41d0befae84f1443b6f665fa471d
Nayeem <nimuzzamanj@gmail.com>
2023-04-07 07:30:00 +0600

    initial commit

---
+++ functions/api.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const cors = require('cors');
+const serverless = require('serverless-http');
+
+const tourRouter = require('../routes/tourRoutes');
+const userRouter = require('../routes/userRoutes');
+const viewRouter = require('../routes/viewRoutes');
+const reviewRouter = require('../routes/reviewRoutes');
+const bookingRouter = require('../routes/bookingRoutes');
+const AppError = require('../utils/appError');
+const globalErrorHandler = require('../controllers/errorController');
+const bookingController = require('../controllers/bookingController');
+
+//start express app
+const app = express();
+
+app.enable('trust proxy');
+
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
+
+//"node": ">=8.3.0" in package.json node engine
+
+// global middleware
+app.use(cors());
+app.options('*',cors());
+//serving static file
+app.use(express.static(path.join(__dirname,'public')));
+//set security http headers
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+        crossOriginEmbedderPolicy: false
+    })
+);
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+// rate limier per ip
+const limiter = rateLimit({
+    max : 100,
+    windowMs : 60*60*1000,
+    message:'you tries many times.Try again after a hour'
+});
+
+app.post('/webhook-checkout',express.raw({type: 'application/json'}),bookingController.webhookCheckout);
+
+app.use('/api',limiter);
+
+
+//middleware
+// body parser ,reading data from body into req.body
+app.use(express.json({limit:'10kb'}));
+app.use(express.urlencoded({extended:true,limit:'10kb'}));
+app.use(cookieParser());
+
+//data sanitize for noSql injection
+app.use(mongoSanitize());
+
+//data sanitize for xss
+app.use(xss());
+
+//prevent parameter pollution
+app.use(hpp({
+    whitelist:['duration','price','difficulty','ratingsQuantity','maxGroupSize','ratingsAverage']
+}));
+
+
+app.use(compression());
+
+// app.use((req,res,next)=>{
+//     // eslint-disable-next-line no-console
+//     console.log("first custom middleware");
+//     next();
+// });
+
+//test middleware
+app.use((req,res,next)=>{
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+
+
+// app.get('/api/v1/tours',getAllTours);
+// app.get('/api/v1/tours/:id',getTour);
+// app.patch('/api/v1/tours/:id',updateTour);
+// app.delete('/api/v1/tours/:id',deleteTour);
+// app.post('/api/v1/tours',createTour);
+
+// ROUTES 
+app.use('/',viewRouter);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users',userRouter);
+app.use('/api/v1/reviews',reviewRouter);
+app.use('/api/v1/bookings',bookingRouter);
+
+
+app.all('*',(req,res,next)=>{
+    // res.status(404).json({
+    //     status:'fail',
+    //     message: `Can't find ${req.originalUrl} on this server`
+    // });
+    const err = new AppError( `Can't find ${req.originalUrl} on this server`,404);
+    next(err);
+});
+
+app.use(globalErrorHandler);
+
+module.exports.handler = serverless(app);
